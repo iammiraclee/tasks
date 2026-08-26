38dbf63fca2663389c0b826b94b1b213c58e9788
Miracle <miraclee@udel.edu>
2026-08-26 16:54:17 -0400

    Added "Hello World" to react website and created the "check" command

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): React.JSX.Element {
                 UD CISC275 with Miracle, React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Hello World Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload.
             </p>
         </div>
     );
